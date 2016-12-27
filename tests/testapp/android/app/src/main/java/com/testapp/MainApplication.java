58f8c28c401c5a71a3e8e5c24999fde3b29a4778
Dave Coates <david.coates@alliancesoftware.com.au>
2016-12-27 12:58:14 +1100

    Tests - Remove unused deps, rn upgrade

--- tests/testapp/android/app/src/main/java/com/testapp/MainApplication.java
+++ tests/testapp/android/app/src/main/java/com/testapp/MainApplication.java
@@ -5,7 +5,6 @@ import android.util.Log;
 
 import com.facebook.react.ReactApplication;
 import com.davecoates.rnfirebasebridge.FirebaseBridgePackage;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,8 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new FirebaseBridgePackage(),
-            new VectorIconsPackage()
+            new FirebaseBridgePackage()
       );
     }
   };
